1155e65cd5874ba4e3be7bb36030fe603fdd1076
Belalalshamiry <belalalshamiry1@gmail.com>
2025-04-25 00:39:40 +0300

    Update script6.js

--- script/script6.js
+++ script/script6.js
@@ -3,8 +3,8 @@ const cards = [
     {name: '2', id: 3}, {name: '2', id: 4},
     {name: '3', id: 5}, {name: '3', id: 6},
     {name: '4', id: 7}, {name: '4', id: 8},
-    {name: '5', id: 9}, {name: '5', id: 10},
-    {name: '6', id: 11}, {name: '6', id: 12},
+    {name: '5', id: 9}, 
+  
 ];
 
 let flippedCards = [];
